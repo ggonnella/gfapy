54e6dd27e362597caf590dd1d1086aaa0af38cb3
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-05-18 15:08:04 +0200

    add doc to optfield.rb

--- lib/gfa/optfield.rb
+++ lib/gfa/optfield.rb
@@ -1,3 +1,26 @@
+#
+# A representation of an optional field of a GFA line.
+# This is one of the fields in the form "XX:Y:Z+" where
+# XX is the two-letter tag name, Y the type, and Z+ the value.
+#
+# Example usage:
+# o = GFA::Optfield.new("AA","Z","xxxx")
+# o.tag # => "AA"
+# o.type # => "Z"
+# o.value # => "xxxx"
+#
+# Automatic casting is done for numeric and array types:
+# GFA::Optfield.new("AA","i","12").value # => 12
+# GFA::Optfield.new("AA","f","1.2").value # => 1.2
+# GFA::Optfield.new("AA","B","c,12,12,12").value # => [12,12,12]
+#
+# Type and tag name are fixed, but values can be edited:
+# o = GFA::Optfield.new("AA","Z","xxxx")
+# o.value = "yyyy"
+#
+# The values are validated according to predefined regular expressions:
+# GFA::Optfield.new("AA","i","1A") # => raises GFA::Optfield::ValueError
+#
 class GFA::Optfield
 
   attr_reader :tag, :type
@@ -16,6 +39,14 @@ class GFA::Optfield
     }
   Separator = ":"
 
+  # Creates a GFA::Optfield instance.
+  #
+  # *Raises*:
+  # - +GFA::Optfield::TagError+ if the tag name is not two letters or
+  #                             one letter and one number
+  # - +GFA::Optfield::TypeError+ if the type is not one of "AifZHB"
+  # - +GFA::Optfield::ValueError+ if the value is not in accordance to the
+  #                                specified type
   def initialize(tag, type, value)
     @tag = tag
     @type = type
@@ -27,11 +58,26 @@ class GFA::Optfield
     "#@tag#{GFA::Optfield::Separator}#@type#{GFA::Optfield::Separator}#@value"
   end
 
+  # Sets the +value+ of the GFA::Optfield.
+  #
+  # *Arguments*:
+  #   - +v-: the value to set; the value must be a string which specifies the
+  #   value in accordance to the +type+; for "i" and "F" values, v may be,
+  #   respectively, an Integer or Float
+  # *Note*: for "H" and "B" no automatic back-conversion of +v+ to string is
+  #  done, thus +gfa.value=gfa.value+ will generally fail in these cases
+  #  (while +gfa.value=gfa.value(false)+ will not)
   def value=(v)
     @value = v.to_s
     validate_value!
   end
 
+  # Get the +value+ of the GFA::Optfield.
+  #
+  # *Arguments*:
+  #   - +cast+, if true, non "Z" and "A" values are casted according to
+  #             their type: "i" and "H" to Integer, "f" to Float and
+  #             "B" to an Array of Integer or Float elements
   def value(cast = true)
     return @value if !cast
     case @type
@@ -89,6 +135,11 @@ class GFA::Optfield::ValueError < ArgumentError; end
 
 class String
 
+  # Creates a GFA::Optfield instance from a String
+  #
+  # *Raises*:
+  #   - +TypeError+ if the String cannot be converted into an Optfield
+  #   - see GFA::Optfield#new for other possible exceptions
   def to_gfa_optfield
     components = split(GFA::Optfield::Separator)
     if components.size != 3
