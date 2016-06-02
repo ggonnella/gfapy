9e79dcd0e524bf918663b4d7c84d900458e8f5c9
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-02 11:08:19 +0200

    split into two gems: traverse.rb

--- lib/gfa/traverse.rb
+++ lib/gfa/traverse.rb
@@ -94,38 +94,7 @@ module GFA::Traverse
     return components
   end
 
-  def remove_p_bubbles
-    visited = Set.new
-    segment_names.each do |sn|
-      next if visited.include?(sn)
-      if connectivity(sn) == [1,1]
-        s1 = neighbours([sn, :B])[0]
-        s2 = neighbours([sn, :E])[0]
-        n1 = neighbours(s1).sort
-        n2 = neighbours(s2).sort
-        n1.each {|se| visited << se[0]}
-        if n1 == n2.map{|se| other_segment_end(se)}
-          remove_proven_p_bubble(s1, s2, n1)
-        end
-      end
-    end
-  end
-
-  def remove_p_bubble(segment_end1, segment_end2)
-    n1 = neighbours(segment_end1).sort
-    n2 = neighbours(segment_end2).sort
-    raise if n1 != n2.map{|se| other_segment_end(se)}
-    raise if n1.any? {|se| connectivity(se[0]) != [1,1]}
-    remove_proven_p_bubble(segment_end1, segment_end2, n1)
-  end
-
   private
-
-  def remove_proven_p_bubble(segment_end1, segment_end2, alternatives)
-    coverages = alternatives.map{|s|segment!(s[0]).coverage}
-    alternatives.delete_at(coverages.index(coverages.max))
-    alternatives.each {|s| delete_segment(s[0])}
-  end
 
   def traverse_component(segment_end, c, visited)
     links_of(segment_end).each do |l|
