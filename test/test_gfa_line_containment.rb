b8aaf39a3f1a00661df27135583b0ea90e9cb485
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-03 18:07:36 +0200

    fix containment test

--- test/test_gfa_line_containment.rb
+++ test/test_gfa_line_containment.rb
@@ -4,7 +4,7 @@ require "test/unit"
 class TestGFALineContainment < Test::Unit::TestCase
 
   def test_from_string
-    fields=["C","1","+","2","-","12","12M","RC:i:1232","NM:i:3","ab:Z:abcd"]
+    fields=["C","1","+","2","-","12","12M","MQ:i:1232","NM:i:3","ab:Z:abcd"]
     str=fields.join("\t")
     assert_nothing_raised { str.to_gfa_line }
     assert_equal(GFA::Line::Containment, str.to_gfa_line.class)
@@ -17,8 +17,8 @@ class TestGFALineContainment < Test::Unit::TestCase
     assert_equal("12", str.to_gfa_line.pos(false))
     assert_equal([[12,"M"]], str.to_gfa_line.overlap)
     assert_equal(fields[6], str.to_gfa_line.overlap(false))
-    assert_equal(1232, str.to_gfa_line.RC)
-    assert_equal("1232", str.to_gfa_line.RC(false))
+    assert_equal(1232, str.to_gfa_line.MQ)
+    assert_equal("1232", str.to_gfa_line.MQ(false))
     assert_equal(3, str.to_gfa_line.NM)
     assert_equal("abcd", str.to_gfa_line.ab)
     assert_raises(TypeError) { (str+"\tH1").to_gfa_line }
@@ -36,7 +36,7 @@ class TestGFALineContainment < Test::Unit::TestCase
       f=fields.dup; f[6]="x"; f.join("\t").to_gfa_line
     end
     assert_raises(GFA::Line::PredefinedOptfieldTypeError) do
-      f=fields.dup; f[7]="RC:Z:1232"; f.join("\t").to_gfa_line
+      f=fields.dup; f[7]="MQ:Z:1232"; f.join("\t").to_gfa_line
     end
     assert_raises(GFA::Line::PredefinedOptfieldTypeError) do
       f=fields.dup; f[8]="NM:Z:1232"; f.join("\t").to_gfa_line
