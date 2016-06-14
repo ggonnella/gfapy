7af96ed5f26871a58299da28a3b85bec69b63f3c
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-14 11:14:04 +0200

    yard docu for connection_info

--- lib/gfa/connection_info.rb
+++ lib/gfa/connection_info.rb
@@ -1,12 +1,31 @@
+#
+# Collection of hashes which allow fast retrieval of the lines of a GFA
+# which refer to a given segment.
+#
+# Note that it is not required that the segment has already been added
+# to the GFA using an "S" line. This is necessary, as the order of the lines
+# in the file during parsing is arbitrary.
+#
 class GFA::ConnectionInfo
 
+  # @return [GFA::ConnectionInfo]
+  # @param [Array] reference to GFA instance @lines array (this is required by
+  #   the #validate! and the #lines methods only)
   def initialize(lines)
     @lines = lines
     @connect = {}
     [:L,:C,:P].each {|rt| @connect[rt] = {}}
   end
 
-  # Add values to connection infos
+  # Add a reference to a link/containment or path to connection infos
+  #
+  # @param rt [:L|:C|:P] the record type
+  # @param value [Integer] an index in @lines[rt]
+  # @param sn [String] the segment name
+  # @param dir [:from|:to|nil] is segment the from or the to segment of the
+  #   link/containment?; use nil for paths
+  # @param o ["+"|"-"|nil] the segment orientation (links/containments); use
+  #   nil for # paths
   def add(rt, value, sn, dir=nil, o=nil)
     rt = rt.to_sym
     sn = sn.to_sym
@@ -27,14 +46,22 @@ class GFA::ConnectionInfo
     self
   end
 
-  # Remove values from connection infos
+  # Remove a link/containment/path reference from connection infos
   #
-  # Usage:
-  # - delete("P", value, sn) => rm path ref
-  # - delete("C"|"L", value, sn, :from|:to, "+"|"-") => rm link/containment ref
-  # - delete("C"|"L", value, sn, :from|:to, nil) => rm link/containment ref
-  #                                                 from both "+" and "-"
-  #                                                 connections of sn
+  # @param rt [:L|:C|:P] the record type
+  # @param value [Integer] the value (index in @lines[rt]) to remove
+  # @param sn [String] the segment name
+  # @param dir [:from|:to|nil] direction (for links/containments);
+  #   set to nil for paths
+  # @param o ["+"|"-"|nil] orientation (for links/containments);
+  #   set to nil for paths; if nil in links/containments: both
+  #
+  # @example delete("P", value, sn) => rm path ref
+  # @example  delete("C"|"L", value, sn, :from|:to, "+"|"-")
+  #   => rm link/containment ref
+  # @example delete("C"|"L", value, sn, :from|:to, nil)
+  #   => rm link/containment ref from both "+" and "-" connections of sn
+  #
   def delete(rt, value, sn, dir=nil, o=nil)
     rt = rt.to_sym
     sn = sn.to_sym
@@ -56,6 +83,9 @@ class GFA::ConnectionInfo
     self
   end
 
+  # Rename a segment in the connection info.
+  # @param sn [String] old segment name
+  # @param new_sn [String] new segment name
   def rename_segment(sn, new_sn)
     sn = sn.to_sym
     new_sn = new_sn.to_sym
@@ -69,13 +99,11 @@ class GFA::ConnectionInfo
     self
   end
 
+  # Delete all information about a segment in the connection info.
+  # @param sn [String] the segment name
   def delete_segment(sn)
     [:P, :L, :C].each {|rt| @connect[rt].delete(sn.to_sym)}
     self
-  end
-
-  def lines(rt, sn, dir = nil, o = nil)
-    find(rt, sn, dir, o).map{|i| @lines[rt][i]}
   end
 
   # Find values from connection infos
@@ -102,12 +130,19 @@ class GFA::ConnectionInfo
     end
   end
 
-  # Checks if all elements of connect refer to a path, link of containment
-  # which was not deleted and whose fields (from, to, from_orient, to_orient
-  # for links and containments, segment_names for paths) have the expected
-  # values.
+  # @return [Array<GFA::Line>] the lines of the given record type for segment
+  def lines(rt, sn, dir = nil, o = nil)
+    find(rt, sn, dir, o).map{|i| @lines[rt][i]}
+  end
+
+  # Validate the information in connection info (useful for debugging).
   #
-  # Method useful for debugging.
+  # @raise if any path/link/containment was deleted from +@lines+
+  # @raise if any link/containment field (from/from_orient/to/to_orient) is not
+  #   consistent with the stored information
+  # @raise if the paths segment name field is not consistent
+  #   with the stored inforation
+  #
   def validate!
     @connect[:P].keys.each do |sn|
       @connect[:P][sn].each do |li|
