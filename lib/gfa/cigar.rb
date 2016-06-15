70aaa3787e0c2ec4ac3d2b989f8a5687e2dde4aa
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-06-15 16:11:51 +0200

    complete documentation of cigar

--- lib/gfa/cigar.rb
+++ lib/gfa/cigar.rb
@@ -8,7 +8,8 @@ module GFA::CIGAR
   # symbol (one of MIDNSHPX=).
   #
   # @return ["*"] if self == "*"
-  # @return [Array<Integer, String(1)>] otherwise
+  # @return [Array<GFA::CigarOperation>] otherwise
+  # @raise [TypeError] if the string is not a valid CIGAR string
   def cigar_operations
     return "*" if self == "*"
     raise TypeError if self !~ /^([0-9]+[MIDNSHPX=])+$/
@@ -19,25 +20,14 @@ module GFA::CIGAR
     end
   end
 
-
   # Parses a CIGAR string representing an overlap and reverses it, i.e.
   # computes the CIGAR for the segments in reverse direction.
-  # Return type: @see cigar_operations
+  # Returns an array of CIGAR operations.
   #
-  # Example of conversion of a link to its reverse complement:
-  #
-  #   S1 + S2 + 2M1D3M
-  #
-  #   S1+  ACGACTGTGA
-  #   S2+      CT-TGACGG
-  #
-  #   S2-  CCGTCA-AG
-  #   S1-     TCACAGTCGT
-  #
-  #   S2 - S1 - 3M1I2M
-  #
+  # @see #reverse_cigar
   # @return ["*"] if self == "*"
-  # @return [Array<Integer, String(1)>] otherwise
+  # @return [Array<GFA::CigarOperation>] otherwise
+  # @raise [TypeError] if the string is not a valid CIGAR string
   def reverse_cigar_operations
     return "*" if self == "*"
     self.cigar_operations.reverse.map do |oplen, opcode|
@@ -50,9 +40,28 @@ module GFA::CIGAR
     end
   end
 
-  # @see reverse_cigar_operation
+  # Parses a CIGAR string representing an overlap and reverses it, i.e.
+  # computes the CIGAR for the segments in reverse direction.
+  #
+  # @example
+  #
+  #   "2M1D3M".reverse_cigar # => "3M1I2M"
+  #
+  #   # S1 + S2 + 2M1D3M
+  #   #
+  #   # S1+  ACGACTGTGA
+  #   # S2+      CT-TGACGG
+  #   #
+  #   # S2-  CCGTCA-AG
+  #   # S1-     TCACAGTCGT
+  #   #
+  #   # S2 - S1 - 3M1I2M
+  #
+  #
+  # @see #reverse_cigar_operations
   # @return ["*"] if self == "*"
   # @return [String] the reverse CIGAR, otherwise
+  # @raise [TypeError] if the string is not a valid CIGAR string
   def reverse_cigar
     return "*" if self == "*"
     reverse_cigar_operations.flatten.join
@@ -62,10 +71,12 @@ end
 
 # Class representing a CIGAR operation
 class GFA::CigarOperation < Array
+  # The operation length
   # @return [Integer] operation length
   def oplen
     self[0]
   end
+  # The operation code
   # @return [String] <i>(length: 1)</i>
   #   operation code
   def opcode
