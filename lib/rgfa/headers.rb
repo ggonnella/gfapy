59cce44ac2027c5877870e6c31b0fb2831779a5d
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-09-01 10:44:15 +0200

    update headers documentation

--- lib/rgfa/headers.rb
+++ lib/rgfa/headers.rb
@@ -4,38 +4,31 @@ require_relative "field_array"
 #
 # Methods for the RGFA class, which allow to handle headers in the graph.
 #
-# It is unclear if only a single header is allowed. If multiple H lines are
-# allowed, then the same tag may be set on different lines.
-# RGFA allows for both possibilities.
 # The header is accessed using #header.
-# This returns a RGFA::Line::Header object.
+# This returns a +RGFA::Line::Header+ object.
 #
-# <b>Single header line, or multiple lines but no repeated tags </b>
-# @example
+# @example Accessing the header information
 #   rgfa.header.VN # => “1.0”
 #   rgfa.header.co = “This the header comment”
 #   rgfa.header.ni = 100
 #   rgfa.header.field_to_s(:ni) # => “ni:i:100”
 #
-# <b> Multiple header lines, with repeated tags: RGFA::FieldArray</b>
+# The specification does not explicitely forbid to have the same tag on
+# different lines. To represent this case, a "field array" is used,
+# which is an array of instances of a tag, from different lines of the header.
 #
-# Repeated tags are represented using a subclass of Array (RGFA::FieldArray):
-# @example
+# @example Header with tags repeated on different lines (see RGFA::FieldArray)
 #   rgfa.header.ni # => RGFA::FieldArray<[100,200] @datatype: :i>
 #   rgfa.header.ni[0] # 100
 #   rgfa.header.ni << 200 # “200” is also OK
 #   rgfa.header.ni.map!{|i|i-10}
 #   rgfa.header.ni = [100,200,300].to_rgfa_field_array
 #
-# <b>RGFA::Line::Header#add</b>
-#
-# Using the header add method, it is possible to add further values of a tag,
-# one at a time:
-#
-# @example:
+# @example Adding instances of a tag (will go on different header lines)
 #   rgfa.header.add(:xx, 100) # => 100 # single i tag, if .xx did not exist yet
 #   rgfa.header.add(:xx, 100) # => RGFA::FieldArray<[100,100] @datatype: :i>
 #   rgfa.header.add(:xx, 100) # => RGFA::FieldArray<[100,100,100] @datatype :i>
+#
 module RGFA::Headers
 
   # @return [RGFA::Line::Header] an header line representing the entire header
@@ -45,24 +38,37 @@ module RGFA::Headers
     @headers
   end
 
-  # Header information of the graph in form of RGFA::Line::Header
-  # objects (each containing a single field of the header).
+  # Header information of the graph in form of an array of RGFA::Line::Header
+  # objects, each containing a single field of the header.
+  # @note The returned array containes copies of the original values,
+  # i.e.\ changes in the lines will not affect the RGFA object; to update the
+  # values in the RGFA use the #header method.
   # @return [Array<RGFA::Line::Header>]
   def headers
     @headers.split
   end
 
+  # Iterator over the header information of the graph, in form of
+  # RGFA::Line::Header objects, each containing a single field of the header.
+  # @note The returned array containes copies of the original values,
+  # i.e.\ changes in the lines will not affect the RGFA object; to update the
+  # values in the RGFA use the #header method.
+  # @return [Array<RGFA::Line::Header>]
   def each_header(&block)
     headers.each(&block)
   end
 
-  # Remove all headers
+  # Remove all information from the header.
   # @return [RGFA] self
   def delete_headers
     init_headers
     return self
   end
 
+  # Add a GFA line to the header. This is useful for constructing the graph.
+  # For adding values to the header, see #header.
+  # @param gfa_line [String, RGFA::Line::Header] a string representing a valid
+  #   header line, or a RGFA header line object
   def add_header(gfa_line)
     gfa_line = gfa_line.to_rgfa_line(validate: @validate)
     @headers.merge(gfa_line)
