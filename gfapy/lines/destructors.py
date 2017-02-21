bb574aea64863307f44367267f3707db8770efff
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2017-02-21 14:52:07 +0100

    [py] make disconnect faster

--- gfapy/lines/destructors.py
+++ gfapy/lines/destructors.py
@@ -15,25 +15,31 @@ class Destructors:
 
   def _unregister_line(self, gfa_line):
     self._api_private_check_gfa_line(gfa_line, "unregister_line")
-    if gfa_line.record_type == "H":
+    rt = gfa_line.record_type
+    if rt == "H":
       raise gfapy.AssertionError("Bug found, please report\n"+
         "gfa_line: {}".format(gfa_line))
-    collection = self._records[gfa_line.record_type]
+    collection = self._records[rt]
     key = gfa_line
     delete_if_empty = None
     if isinstance(collection,dict):
       storage_key = gfa_line.__class__.STORAGE_KEY
       if storage_key == "name":
-        if not gfapy.is_placeholder(gfa_line.name):
-          key = gfa_line.name
+        name = gfa_line.name
+        if not gfapy.is_placeholder(name):
+          collection.pop(name)
+          return
         else:
           collection = collection[None]
       elif storage_key == "external":
-        collection = collection[gfa_line.external.name]
         delete_if_empty = gfa_line.external.name
-    if isinstance(collection,list):
-      collection[:] = [line for line in collection if line is not key]
-    else:
-      collection.pop(key)
+        collection = collection[delete_if_empty]
+    found = False
+    for i, line in enumerate(collection):
+      if line is key:
+        found = i
+        break
+    assert(found is not False)
+    collection.pop(found)
     if delete_if_empty and not collection:
-      self._records[gfa_line.record_type].pop(delete_if_empty)
+      self._records[rt].pop(delete_if_empty)
