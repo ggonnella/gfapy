d80ce98504ee246eb90cd1079f282c74e3cf05f7
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2020-04-18 20:18:42 +0200

    add some tests using examples from github

---
+++ tests/test_api_rgfa.py
@@ -0,0 +1,36 @@
+import gfapy
+import unittest
+
+class TestAPIrGfa(unittest.TestCase):
+
+  def test_adding_invalid_segment_to_rgfa(self):
+    gfa = gfapy.Gfa()
+    gfa.append("S\t1\t*")
+    gfa.validate()
+    gfa = gfapy.Gfa(dialect="rgfa")
+    gfa.append("S\t1\t*")
+    with self.assertRaises(gfapy.NotFoundError): gfa.validate()
+
+  def test_adding_containment_to_rgfa(self):
+    gfa = gfapy.Gfa()
+    gfa.append("C\t1\t+\t2\t+\t12\t*")
+    gfa.validate()
+    gfa = gfapy.Gfa(version="gfa1",dialect="rgfa")
+    gfa.append("C\t1\t+\t2\t+\t12\t*")
+    with self.assertRaises(gfapy.NotFoundError): gfa.validate()
+
+  def test_loading_examples(self):
+    with self.assertRaises(gfapy.FormatError):
+      gfapy.Gfa.from_file("tests/testdata/rgfa_example.1.gfa")
+    gfapy.Gfa.from_file("tests/testdata/rgfa_example.1.gfa", dialect="rgfa")
+    with self.assertRaises(gfapy.FormatError):
+      gfapy.Gfa.from_file("tests/testdata/rgfa_example.2.gfa")
+    gfapy.Gfa.from_file("tests/testdata/rgfa_example.2.gfa", dialect="rgfa")
+
+  def test_stable_sequence_names(self):
+    g = gfapy.Gfa.from_file("tests/testdata/rgfa_example.2.gfa", dialect="rgfa")
+    self.assertEqual(['smpl-Ref.Bd4', 'smpl-Bd21_3_r.pseudomolecule_4'],
+        g.stable_sequence_names())
+    g = gfapy.Gfa.from_file("tests/testdata/rgfa_example.1.gfa", dialect="rgfa")
+    self.assertEqual(['bar', 'foo', 'chr1'],
+        g.stable_sequence_names())
